a86ee90c50173a563054900a63f3770f1aed3570
unknown <mchlcape808@gmail.com>
2019-05-11 19:16:39 -0700

    solved #2

--- exercises.js
+++ exercises.js
@@ -51,6 +51,13 @@ console.log(canVote(19));
  * Console.log your result.
 */
 
+function login(password){
+  if (password === 'test1234'){
+    return 'login success!'
+  }
+}
+
+login(test1234);
 
 /*
  * #3
